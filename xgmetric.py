e6ac60832703d36f291e34d9f04ee8afe8272ec5
Pu Xiao <puxiao10@gmail.com>
2023-09-19 15:27:51 -0400

    basic model 9/19

---
+++ xgmetric.py
@@ -0,0 +1,24 @@
+"""
+this file is used for xG model evaluation
+"""
+
+def evaluate(
+    pred,
+    y,
+    threshold = 0.5
+):
+    
+    # print out AUC / recall / precision
+
+    # print out confusion matrix
+
+    pass
+
+def compare_scatter(
+    pred,
+    y,
+    xG
+):
+    # draw the scatter plot
+
+    pass
